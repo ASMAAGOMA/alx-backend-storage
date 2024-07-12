61b914f4ed27fef59848090dfbaefd4767083591
asmaagoma <asmaagadallaah@gmail.com>
2024-07-12 04:09:53 +0300

    :"(

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,2 @@
 -- Add column for first letter
-ALTER TABLE names 
-ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
-
--- Create index on first_letter column
-CREATE INDEX idx_name_first ON names (first_letter);
+CREATE INDEX first ON names (name(1));
