d97da878d3594976e3ca2869ef0829e04277759f
asmaagoma <asmaagadallaah@gmail.com>
2024-07-12 04:39:05 +0300

    :"(

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,22 +1,15 @@
 -- function
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 4)
+RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL(10, 4);
-
-    -- Handle NULL inputs
-    IF a IS NULL OR b IS NULL THEN
-        SET result = NULL;  -- or handle as appropriate
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
     ELSE
-        IF b = 0 THEN
-            SET result = 0;
-        ELSE
-            SET result = a / b;
-        END IF;
+        SET result = a / b;
     END IF;
-
     RETURN result;
 END$$
 DELIMITER ;
